9ed385be82cd6dfc8ca1b22c0dc2ca77e8d404ca
MarinaKopat <marinakopat@gmail.com>
2026-02-10 10:01:17 +0300

    Files

---
+++ conftest.py
@@ -0,0 +1,25 @@
+
+@pytest.fixture(autouse=False, params=["chrome"])
+def driver(request):
+    if request.param == "firefox":
+        opts = FirefoxOptions()
+        opts.add_argument("--width=1980")
+        opts.add_argument("--height=1600")
+        web_driver = webdriver.Firefox(options=opts)
+
+    else:
+        opts = Options()
+        # opts.add_argument("--headless=new")
+        web_driver = webdriver.Chrome(options=opts)
+        web_driver.maximize_window()
+        web_driver.implicitly_wait(3)
+
+    yield web_driver
+    web_driver.quit()
+
+
+@pytest.fixture(scope="module", autouse=False)
+def auto_use():
+    print("\n+++Вызвали фикстуру 'auto_use'+++")
+    yield {"name": "Alica"}
+    print("\n+++Вернулись в фикстуру 'auto_use'+++")
