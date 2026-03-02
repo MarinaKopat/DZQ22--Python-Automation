12c42e8f5bd1a89340fc5efa7f99278d8c61f695
MarinaKopat <marinakopat@gmail.com>
2026-03-02 12:16:01 +0300

    Files

---
+++ tests/test_file_dz.py
@@ -0,0 +1,37 @@
+import time
+import os
+import pytest
+from selenium.webdriver.common.by import By
+
+URL = 'https://demoqa.com/upload-download'
+
+
+@pytest.mark.download
+def test_download(driver):
+    url = 'https://demoqa.com/upload-download'
+    driver.get(url)
+    file_name = 'sampleFile.jpeg'
+    driver.find_element(By.ID, 'downloadButton').click()
+    time.sleep(6)
+
+    driver.get('chrome://downloads/')
+    assert file_name == 'sampleFile.jpeg'
+
+
+URL = 'https://demoqa.com/upload-download'
+
+
+@pytest.mark.upload
+def test_upload(driver):
+    driver.get(URL)
+    driver.find_element(By.ID, 'uploadFile').send_keys(os.path.abspath
+    ('C:/Users/user/Desktop/DZQ22--Python-Automation/file/sampleFile.jpeg'))
+    time.sleep(2)
+
+    result = driver.find_element(By.ID, 'uploadedFilePath')
+    assert result.is_displayed()
+    assert 'sampleFile.jpeg' in result.text
+
+
+
+
---
+++ tests/test_frame_dz.py
@@ -0,0 +1,16 @@
+import time
+import pytest
+from selenium.webdriver.common.by import By
+
+URL = "https://demoqa.com/frames"
+
+
+@pytest.mark.frame
+def test_frame(driver):
+    driver.get(URL)
+    time.sleep(5)
+    driver.switch_to.frame(driver.find_element(By.ID, 'frame1'))
+    text = driver.find_element(By.CSS_SELECTOR, '#sampleHeading')
+    assert text.text == 'This is a sample page'
+
+    driver.switch_to.default_content()
